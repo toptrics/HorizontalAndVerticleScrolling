c3507a9d378a95f16c8cbec0c5eccddf36b50cdf
Sunil Sharma <ksharmasunil54@gmail.com>
2022-08-03 16:28:29 +0530

    Initial Commit

---
+++ HorizontalAndVerticleScrolling/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HorizontalAndVerticleScrolling
+//
+//  Created by Sunil Sharma on 03/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
